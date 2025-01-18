e6e9807ffbc5e238c0fff17578db88cf2aa35deb
gm50x <getuliomagela@outlook.com>
2025-01-18 18:45:55 -0300

    feat: adds s3 localstack

--- apps/fiap-x-api/src/video/infra/infra.module.ts
+++ apps/fiap-x-api/src/video/infra/infra.module.ts
@@ -1,6 +1,5 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { StorageModule } from '@fiap-x/storage';
 import { Module } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { StorageService } from '../application/abstractions/storage.service';
 import { VideoRepository } from '../application/abstractions/video.repository';
@@ -10,7 +9,7 @@ import {
   MongooseVideoSchema,
   MongooseVideoSchemaModel,
 } from './persistence/mongoose/video.schema';
-import { AwsS3StorageService } from './storage/aws-s3/aws-s3-storage.service';
+import { AwsS3VideoStorageService } from './storage/aws-s3/aws-s3-storage.service';
 
 const MongooseSchemaModule = MongooseModule.forFeature([
   {
@@ -22,7 +21,7 @@ const MongooseSchemaModule = MongooseModule.forFeature([
 MongooseSchemaModule.global = true;
 
 @Module({
-  imports: [MongooseSchemaModule],
+  imports: [StorageModule, MongooseSchemaModule],
   providers: [
     MongooseVideoSchemaFactory,
     {
@@ -31,21 +30,7 @@ MongooseSchemaModule.global = true;
     },
     {
       provide: StorageService,
-      useClass: AwsS3StorageService,
-    },
-    {
-      provide: S3Client,
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return new S3Client({
-          region: config.get('AWS_REGION'),
-          credentials: {
-            accessKeyId: config.get('AWS_ACCESS_KEY_ID'),
-            secretAccessKey: config.get('AWS_SECRET_ACCESS_KEY'),
-            sessionToken: config.get('AWS_SESSION_TOKEN'),
-          },
-        });
-      },
+      useClass: AwsS3VideoStorageService,
     },
   ],
   exports: [VideoRepository, StorageService],
--- apps/fiap-x-api/src/video/infra/storage/aws-s3/aws-s3-storage.service.ts
+++ apps/fiap-x-api/src/video/infra/storage/aws-s3/aws-s3-storage.service.ts
@@ -1,4 +1,4 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { AwsS3StorageService } from '@fiap-x/storage';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
@@ -7,9 +7,9 @@ import {
 } from '../../../application/abstractions/storage.service';
 
 @Injectable()
-export class AwsS3StorageService implements StorageService {
+export class AwsS3VideoStorageService implements StorageService {
   constructor(
-    private readonly s3Client: S3Client,
+    private readonly client: AwsS3StorageService,
     private readonly config: ConfigService,
   ) {}
 
@@ -18,17 +18,10 @@ export class AwsS3StorageService implements StorageService {
     content: Buffer<ArrayBufferLike>,
   ): Promise<UploadVideoResult> {
     const bucket = this.config.get('AWS_S3_BUCKET_NAME');
-    const provider = 'AWS::S3';
-    const command = new PutObjectCommand({
-      Bucket: bucket,
-      Key: path,
-      Body: content,
-    });
-    await this.s3Client.send(command);
-    return {
-      provider,
+    return this.client.uploadFile({
       bucket,
       path,
-    };
+      content,
+    });
   }
 }
