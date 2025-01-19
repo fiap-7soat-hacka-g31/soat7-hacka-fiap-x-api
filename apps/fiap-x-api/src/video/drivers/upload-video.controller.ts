8b6c1b17a8555a000ea560ab048a7ec091488952
gm50x <getuliomagela@outlook.com>
2025-01-19 10:52:58 -0300

    adds validation

--- apps/fiap-x-api/src/video/drivers/upload-video.controller.ts
+++ apps/fiap-x-api/src/video/drivers/upload-video.controller.ts
@@ -1,5 +1,8 @@
 import {
   Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
   UploadedFile,
   UseInterceptors,
@@ -8,13 +11,25 @@ import { CommandBus } from '@nestjs/cqrs';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UploadVideoCommand } from '../application/commands/upload-video.command';
 
+const ONE_HUNDRED_MEGABYTES = 1000 * 1000 * 100;
+
 @Controller({ version: '1', path: 'videos' })
 export class UploadVideoController {
   constructor(private readonly commandBus: CommandBus) {}
 
   @UseInterceptors(FileInterceptor('file'))
   @Post('upload')
-  async execute(@UploadedFile() file: Express.Multer.File) {
+  async execute(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: ONE_HUNDRED_MEGABYTES }),
+          new FileTypeValidator({ fileType: 'video/mp4' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     const result = await this.commandBus.execute(
       new UploadVideoCommand({
         ownerId: '6592008029c8c3e4dc76256c',
