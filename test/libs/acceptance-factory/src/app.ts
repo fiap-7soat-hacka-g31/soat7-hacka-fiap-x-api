7c62da947eccfe75027d431dd968b9910dfbe706
gm50x <getuliomagela@outlook.com>
2025-01-22 19:45:57 -0300

    cleanup

--- test/libs/acceptance-factory/src/app.ts
+++ test/libs/acceptance-factory/src/app.ts
@@ -6,6 +6,7 @@ let appPromise: Promise<INestApplicationContext>;
 export function startApp(rootModule: Type) {
   appPromise = NestFactory.createApplicationContext(rootModule).catch(
     (error) => {
+      // eslint-disable-next-line no-console
       console.error(error);
       process.exit(1);
     },
